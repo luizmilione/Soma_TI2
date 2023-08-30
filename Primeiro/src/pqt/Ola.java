b2de3b3a849909a63058a9a5760a7f31eb058a7d
Luiz Gabriel Milione Assis <143033879+luizmilione@users.noreply.github.com>
2023-08-30 15:26:26 -0300

    Update Ola.java
    Comentando melhor o código

--- Primeiro/src/pqt/Ola.java
+++ Primeiro/src/pqt/Ola.java
@@ -4,13 +4,20 @@ import java.util.Scanner;
 public class Ola {
 public static void main (String [] args)
 {
-	int a, b;
+	int a, b, soma;
 	Scanner input = new Scanner (System.in);
  	System.out.println("Ola TI2!");
+	
+ 	//Leitura do primeiro número:
  	System.out.println("Digite um número");
  	a = input.nextInt();
+	
+ 	//Leitura do segundo número:
  	System.out.println("Digite outro número");
  	b = input.nextInt();
- 	System.out.printf("A soma e %d", a+b);
+	
+	//Somando e exibindo o resultado:
+ 	soma = a + b;
+ 	System.out.printf("A soma e %d", soma);
 }
 }
